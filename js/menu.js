e1d66e7befc83ef26766961027a8bf2221e8d328
= <=>
2020-12-15 09:48:44 +0900

    1215 임태주 menu.js hover기능 변경 및 관리자메인 padding 통일

--- js/menu.js
+++ js/menu.js
@@ -4,17 +4,18 @@ $(function () {
     });
     
     $("#sub_menu").mouseleave(function(e) {
-        var showid = $(this).attr("href");
+        $(".menu_tab").removeClass("bg_white");
+        
         $("#sub_menu").removeClass("sub_menu");
         $(".menu").hide();
     });
-    $(".menu_tab").mouseleave(function(e) {
-        var showid = $(this).attr("href");
-    });
 
     $(".menu_tab").mouseover(function(e) {
+        $(".menu_tab").removeClass("bg_white");
         $(".menu").hide();
+
         var showid = $(this).attr("href");
+        $(this).addClass("bg_white");
         $("#sub_menu").addClass("sub_menu");
         $(showid).show();
     }); 
